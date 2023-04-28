1c44b421ef5f54d4002f43182fb7e78443639147
Benja153 <goosta19802@gmail.com>
2023-04-28 14:36:59 -0300

    primer commit

---
+++ src/Components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../Styles/header.css'
+const Header = () => {
+  return (
+    <h1 className='textoHeader'>Header</h1>
+
+  )
+}
+
+export default Header
---
+++ src/Components/Principal.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Principal = () => {
+  return (
+    <div>
+
+        <h1>
+                ESTE ES EL CONTENIDO PRINCIPAL
+        </h1>
+    </div>
+  )
+}
+
+export default Principal
